f6a28337de0a64c7640ff3446fd0f7d97808f9de
Naman <snamanshah@gmail.com>
2023-09-25 00:40:52 +0530

    added middlewares in file.js

--- learning/file.js
+++ learning/file.js
@@ -1,10 +1,38 @@
+// importing required modules
+import {dirname} from "path";
 import express from "express";
-const app = express();
-const port = 3000;
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
-app.get('/',(req,res)=>{
-    res.send("hello world!");
-})
+// configuring path to the root folder
+const __dirname = dirname(fileURLToPath(import.meta.url)); 
+const app = express(); 
+const port = 3000;
+var bandname = "";
+
+// middleware function to generate band name
+function bandNameGenerator(req,res,next){
+    console.log(req.method);
+    bandname = req.body["streets"]+" "+req.body["petnames"];
+    next();
+}
+// calling middlewares before route methods so that they can respond to each reqeuest
+app.use(bodyParser.urlencoded({extended :true}));
+app.use(bandNameGenerator);
+
+// configured port
 app.listen(port,()=>{
-    console.log(`Example app listening on ${port}`);
+    console.log(`server is running on port ${port}`)
 })
+// route methods
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/submit",(req,res,next)=>{
+    res.send(`<h1>your bandname is ${bandname}</h1>`);
+    res.sendStatus(200);
+})
+
+
