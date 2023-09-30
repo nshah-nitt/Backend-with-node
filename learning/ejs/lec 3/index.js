00cf35c34db7a0eaa7ae942413d572196c5ce8aa
Naman <snamanshah@gmail.com>
2023-10-01 02:15:37 +0530

    Added partials and done routing for get methods using ejs

--- learning/ejs/lec 3/index.js
+++ learning/ejs/lec 3/index.js
@@ -1,14 +1,33 @@
 import express from "express";
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static("public"));
+app.use(express.urlencoded({"extended":true}));
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 Step 2: Make sure that static files are linked to and the CSS shows up.
 Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+app.get("/",(req,res)=>{
+  res.render("index.ejs");
+})
+app.get("/about",(req,res)=>{
+  res.render("about.ejs",{
+    path:__dirname
+  });
+});
+app.get("/contact",(req,res)=>{
+  res.render("contact.ejs",{
+    path:__dirname
+  })
+});
+app.get("/#",(req,res)=>{
+  res.render("about.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
