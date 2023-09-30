b5028776a926425868848e667c91023255741220
Naman <snamanshah@gmail.com>
2023-09-30 23:38:08 +0530

    Added logic to render data using ejs

--- learning/ejs/lec 1/index.js
+++ learning/ejs/lec 1/index.js
@@ -1,7 +1,8 @@
+import bodyParser from "body-parser";
 import express from "express";
 const app = express();
 const port = 3000;
-
+app.use(bodyParser.urlencoded({"extended":true}))
 app.get("/", (req, res) => {
   const data = {
     title: "EJS Tags",
@@ -9,8 +10,13 @@ app.get("/", (req, res) => {
     items: ["apple", "banana", "cherry"],
     htmlContent: "<strong>This is some strong text</strong>",
   };
-  res.render("index.ejs", data);
+  res.render("abc.ejs", data);
 });
+app.post("/submit",(req,res)=>{
+  res.render("abc.ejs",{
+    name:req.body["Name"]
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
